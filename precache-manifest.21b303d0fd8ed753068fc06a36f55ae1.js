75423c17d5a621ac0f9415a9b8b2b83f805665ac
HoangThanh <fiwallsld@hoangthanh.0.43.168.192.in-addr.arpa>
2023-11-27 03:28:08 +0700

    Updates

---
+++ precache-manifest.21b303d0fd8ed753068fc06a36f55ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb8f66d7fdf558d009f960a1e452aba",
+    "url": "/ecommerce-admin/index.html"
+  },
+  {
+    "revision": "99a8d0865ebb2e017b4a",
+    "url": "/ecommerce-admin/static/css/main.0c066391.chunk.css"
+  },
+  {
+    "revision": "6aa69e5dac04bc06d115",
+    "url": "/ecommerce-admin/static/js/2.5a6eae2e.chunk.js"
+  },
+  {
+    "revision": "8573fa514b0fed5de415",
+    "url": "/ecommerce-admin/static/js/3.39e71fec.chunk.js"
+  },
+  {
+    "revision": "99a8d0865ebb2e017b4a",
+    "url": "/ecommerce-admin/static/js/main.586d4311.chunk.js"
+  },
+  {
+    "revision": "18967335412921a0d2b6",
+    "url": "/ecommerce-admin/static/js/runtime~main.dab03974.js"
+  }
+]);
